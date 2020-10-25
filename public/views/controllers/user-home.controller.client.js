0382152747c4bf78e0e7a2ad303ebdb8598dbd51
supriyameduri9 <supriya.meduri@sjsu.edu>
2020-10-25 14:33:38 -0700

    fix url typo

--- public/views/controllers/user-home.controller.client.js
+++ public/views/controllers/user-home.controller.client.js
@@ -99,7 +99,7 @@
 		}
 
 		function logout() {
-			$window.location.href = "https://myclouprojectsupriyameduri.com/";
+			$window.location.href = "https://mycloudprojectsupriyameduri.com/";
 		}
 
 	}
